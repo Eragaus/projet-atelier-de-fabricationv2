a72e8327e1c1781351de6b9330c5d986df4f84ae
Eragaus <bryan.joug@gmail.com>
2025-05-24 17:48:59 +0200

    SuiviMaintenance

--- src/main/java/INSA/TD/utils/StringConverterUtils.java
+++ src/main/java/INSA/TD/utils/StringConverterUtils.java
@@ -5,9 +5,25 @@ import INSA.TD.models.Operation;
 import INSA.TD.models.etat.machine.EtatMachine;
 import javafx.util.StringConverter;
 
+import java.time.LocalDate;
+
 public class StringConverterUtils {
 
     private StringConverterUtils() {
+    }
+
+    public static StringConverter<LocalDate> toLocalDateStringConverter() {
+        return new StringConverter<>() {
+            @Override
+            public String toString(LocalDate date) {
+                return date != null ? date.format(ConstantesUtils.DATE_FORMATTER) : "";
+            }
+
+            @Override
+            public LocalDate fromString(String string) {
+                return (string == null || string.isEmpty()) ? null : LocalDate.parse(string, ConstantesUtils.DATE_FORMATTER);
+            }
+        };
     }
 
     public static StringConverter<EtatMachine> toEtatMachineStringConverter() {
